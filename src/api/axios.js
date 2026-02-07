ab91167de57b31f513b1e50deb3bd7b76ea27c73
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2026-02-07 10:05:11 +0800

    minor fixes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/",
-  /* baseURL: "/api", */
+  /* baseURL: "http://localhost:3000/", */
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
